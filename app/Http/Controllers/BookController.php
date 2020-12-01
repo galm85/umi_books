0304a289078acc2a75cc6ea2a52f1b5ca0ed04a1
galm85 <galm85@gmail.com>
2020-12-01 22:58:44 +0200

    fixin integer

--- app/Http/Controllers/BookController.php
+++ app/Http/Controllers/BookController.php
@@ -21,10 +21,10 @@ class BookController extends Controller
         $book->book_name = request('book_name');
         $book->books_number = request('books_number');
         $book->pages_number = request('pages_number');
-        $book->copies_number = request('books_number')/4;
+        $book->copies_number = ceil(request('books_number')/4);
         $book->total_pages_to_print = ceil((request('books_number')/4)*(request('pages_number')/2));
         $book->books_printed = 0;
-        $book->covers_number = request('books_number')/2;
+        $book->covers_number = ceil(request('books_number')/2);
         $book->covers_printed = 0;
         $book->spine = request('spine');
 
